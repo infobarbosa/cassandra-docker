87b21edaed08a27d63f1555823018d9077a1d168
infobarbosa <infobarbosa@yahoo.com.br>
2017-08-02 22:57:31 -0300

    Adicionando IP estático e subnet onde a solução vai rodar.

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,6 @@
-docker run -d --name cassandra infobarbosa/cassandra:standalone
+docker run -d \
+      --name cassandra \
+      --ip 172.18.0.2 \
+      --net bigdata \
+      infobarbosa/cassandra:standalone
+
