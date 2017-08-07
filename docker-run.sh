a5cfa9060224fdab2c5e27bb20410997e51952ce
infobarbosa <infobarbosa@yahoo.com.br>
2017-08-06 23:10:22 -0300

    alterado o docker-run para usar a versão da comunidade.

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,14 @@
 docker run -d \
-      --name cassandra \
-      --ip 172.18.0.2 \
-      --net bigdata \
-      infobarbosa/cassandra:standalone
+	--name cassandra \
+	--net bigdata \
+	--ip 172.18.0.2 \
+	--env RPC_ADDRESS="172.18.0.2" \
+	--env BROADCAST_RPC_ADDRESS="1.2.3.4" \
+	-p "7000:7000" \
+	-p "9042:9042" \
+	-p "7001:7001" \
+	-p "7199:7199" \
+	-p "9160:9160" \
+	-p "9142:9142" \
+	cassandra
 
